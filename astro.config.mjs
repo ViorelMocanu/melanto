de4c7a6692854256c67d8e7be071de4cb1b551e5
Viorel Mocanu PTT <viorel.mocanu@pentest-tools.com>
2024-10-17 19:46:31 +0300

    chore: update deps, add sharp but don't use it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { ENV, LANGUAGE_EXTENDED, SITE_DESCRIPTION, SITE_NAME, ACCENT_COLOR, URL, DEBUG } from './src/config';
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import { astroCSPHashGenerator } from './src/utils/csp-hash';
 import { fileURLToPath } from 'url';
 import path from 'path';
@@ -23,6 +23,7 @@ export default defineConfig({
 		// '/old': '/new',
 	},
 	image: {
+		service: passthroughImageService(),
 		remotePatterns: [{
 			protocol: 'https'
 		}]
